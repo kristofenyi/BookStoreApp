8f7be9786f44c4032e1f3ab28a968731143d11a4
3p1demicz <3p1demicz@gmail.com>
2022-06-10 10:13:24 +0200

    Přidání SeQ a Serilog

--- BookStoreApp.API/Controllers/WeatherForecastController.cs
+++ BookStoreApp.API/Controllers/WeatherForecastController.cs
@@ -21,13 +21,24 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        _logger.LogInformation("Made call to Weather endpoint");
+        try
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            throw new Exception("This is our loggin test Exception");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Fatal Error occured");
+            throw;
+        }
+        
     }
 }
 
--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Host.UseSerilog()
+builder.Host.UseSerilog((ctx, lc) =>
+lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
